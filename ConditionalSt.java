2f81753ca81cdf46124f163777ee5d31a20f841c
Bhumi Singh <bhumisingh.tech@gmail.com>
2026-03-24 05:39:19 +0000

    Conditional Statement

---
+++ ConditionalSt.java
@@ -0,0 +1,17 @@
+public class ConditionalSt {
+    public static void main (String args[]) {
+        // int n = 1;
+        // int result;
+        // result = n>3?3:4;
+        // System.out.println(result);
+
+        String day = "Thur";
+        String result;
+        switch(day) {
+            case "mon": result ="3 am";
+            default: result ="5 am";
+        }
+        System.out.println(result);
+        
+    }
+}
